6cc1b5e2c8a1e9aa2a0d4b7e8a8b147ae156a67c
zainab <damascena1489@gmail.com>
2023-09-12 20:40:22 +0300

    halfway done

--- challenge1.js
+++ challenge1.js
@@ -3,7 +3,11 @@
  * Create a function named `printName`
  * - that just prints your name on the screen
  */
-
+function printName(name){
+    // let name= "zainab"
+    console.log(`my name is ${name }`)
+}
+printName("zanab")
 /**
  * Task 2:
  * Create a function named `printAge`
@@ -11,6 +15,11 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+function printAge(birthYear){
+    console.log(2023 - birthYear)
+}
+
+printAge(2000)
 
 /**
  * Task 3:
@@ -23,9 +32,33 @@
  * -- tr: it should print `Merhaba NAME`
  */
 
+function printHello(name, lang){
+
+    if(lang == "en"){
+        console.log(`Hello ${name}`)
+    }else if(lang == "es"){
+        console.log(`Hola ${name}`)
+    }else if(lang == "fr"){
+        console.log(`Bonjour ${name}`)
+    }else if(lang == "tr"){console.log(`Merhaba ${name}`)}
+
+
+}
+
+
+printHello("zenab", "tr")
 /**
  * Task 4:
  * Create a function named `printMax`
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+function printMax(x,y){
+    if(x>y){
+        console.log(x)
+    }else if (y>x){
+        console.log(y)
+    }
+}
+
+printMax(10,4)
